bdcc44803418555ec42323b9686cbfdd63b4e8be
belindaalvarado <alvaradobelinda653@gmail.com>
2024-10-08 13:45:52 -0500

    seperate login page

--- yt-reactjs-firebase-recipe-app-master/yt-reactjs-firebase-recipe-app-master/src/components/auth/SignIn.jsx
+++ yt-reactjs-firebase-recipe-app-master/yt-reactjs-firebase-recipe-app-master/src/components/auth/SignIn.jsx
@@ -1,18 +1,20 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState} from "react";
 import { auth } from "../../firebase.config";
-
+import { useNavigate } from "react-router-dom";
 
 const SignIn = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
     const signIn = (e) =>{
         e.preventDefault();
         signInWithEmailAndPassword(auth, email, password)
          .then((userCredential) => {
             const user = userCredential.user;
-            console.log("User signed in: ", user)
+            console.log("User signed in: ", user);
+            navigate("/"); 
         }).catch((error) => {
             console.log(error.code, error.message)
         })
@@ -22,20 +24,28 @@ const SignIn = () => {
     return(
         <div className='sign-in-container'>
             <form onSubmit={signIn}>
-                <h1>Log In to you Account</h1>
+                <h1 className="mt-10 text-2xl font-bold">Log In to your Account</h1>
+                <div className=" flex flex-col items-center">
                 <input 
                 type="email" 
                 placeholder="Enter your email" 
                     value={email}
                     onChange={(e) =>setEmail(e.target.value)}
+                    className="block w-80 p-2 border rounded-md "
                 ></input>
                 <input 
                 type="password" 
                 placeholder="Enter your password" 
                     value={password}
                     onChange={(e) =>setPassword(e.target.value)}
+                    className="block w-80 p-2 border rounded-md"
                 ></input>
-                <button type="submit">Log In</button>
+                </div>
+                <button 
+                type="submit"
+                className="py-2 px-4 rounded-md hover:bg-blue-400 hover:text-white"
+                
+                >Log In</button>
             </form>
         </div>
     )
--- yt-reactjs-firebase-recipe-app-master/yt-reactjs-firebase-recipe-app-master/src/components/auth/SignUp.jsx
+++ yt-reactjs-firebase-recipe-app-master/yt-reactjs-firebase-recipe-app-master/src/components/auth/SignUp.jsx
@@ -1,18 +1,21 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState} from "react";
 import { auth } from "../../firebase.config";
+import { useNavigate } from "react-router-dom";
 
 
 const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
     const signUp = (e) =>{
         e.preventDefault();
         createUserWithEmailAndPassword(auth, email, password)
          .then((userCredential) => {
             const user = userCredential.user;
-            console.log("User signed up: ", user)
+            console.log("User signed up: ", user);
+            navigate("/");
         }).catch((error) => {
             console.log(error.code, error.message)
         })
@@ -22,20 +25,28 @@ const SignUp = () => {
     return(
         <div className='sign-in-container'>
             <form onSubmit={signUp}>
-                <h1>Create Account</h1>
+                <h1 className="mt-10 text-2xl font-bold">Create Account</h1>
+                <div className=" flex flex-col items-center">
                 <input 
                 type="email" 
                 placeholder="Enter your email" 
                     value={email}
                     onChange={(e) =>setEmail(e.target.value)}
+                    className="block w-80 p-2 border rounded-md "
                 ></input>
                 <input 
                 type="password" 
                 placeholder="Enter your password" 
                     value={password}
                     onChange={(e) =>setPassword(e.target.value)}
+                    className="block w-80 p-2 border rounded-md "
                 ></input>
-                <button type="submit">Sign Up</button>
+                </div>
+                <button 
+                type="submit"
+                className="py-2 px-4 rounded-md hover:bg-blue-400
+                 hover:text-white"
+                >Sign Up</button>
             </form>
         </div>
     )
